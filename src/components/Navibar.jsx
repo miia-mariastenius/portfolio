601fb198bd5d76b68e15374eedc5dd06cd6d295c
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-06-17 16:10:33 +0300

    Deleted mobile burger nav, added styling to navbar

--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,30 +1,23 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 function Navibar() {
 
   return (
-    <Navbar expand="lg" className="">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="m-auto">
-            <LinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/projects">
-              <Nav.Link>Projects</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/contact">
-              <Nav.Link>Contact</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/accessibility-stetament">
-              <Nav.Link>Accessibility</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Nav variant="pills" className="m-auto my-2 justify-content-center">
+      <LinkContainer to="/">
+        <Nav.Link>Home</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/projects">
+        <Nav.Link>Projects</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/contact">
+        <Nav.Link>Contact</Nav.Link>
+      </LinkContainer>
+      <LinkContainer to="/accessibility-stetament">
+        <Nav.Link>Accessibility</Nav.Link>
+      </LinkContainer>
+    </Nav>
   )
 }
 
