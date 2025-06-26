2bb3a10836cccd0e72efe155f9163f483b5d2741
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-06-26 15:37:15 +0300

    Accessibility statement created and styled

--- src/pages/AccessibilityStatement.jsx
+++ src/pages/AccessibilityStatement.jsx
@@ -1,6 +1,39 @@
 function AccessibilityStatement() {
-  return(
-    <></>
+  return (
+    <>
+        <div className="header-container">
+          <div className="contents">
+            <h1>Accessibility Statement for Miia-Maria Stenius Portfolio</h1>
+            <p>
+              This is an accessibility statement from Miia-Maria Stenius Portfolio.
+            </p>
+            <h2 className="mt-3">Conformance status</h2>
+            <p>
+              The <a href="https://www.w3.org/WAI/standards-guidelines/wcag/">Web Content Accessibility Guidelines (WCAG)</a> defines requirements for designers and developers to improve accessibility for people with disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA. Miia-Maria Stenius Portfolio is partially conformant with WCAG 2.1 level AA. Partially conformant means that some parts of the content do not fully conform to the accessibility standard.
+            </p>
+          </div>
+        </div>
+        <div className="secondary-container">
+          <div className="contents">
+            <h2>Feedback</h2>
+            <p>
+              We welcome your feedback on the accessibility of Miia-Maria Stenius Portfolio.
+              Please let us know if you encounter accessibility barriers on Miia-Maria Stenius Portfolio:
+            </p>
+            <ul className="basic-information feedback h-card">
+              <li>
+                E-mail:&nbsp;
+                <a className="email u-email" href="mailto:n3stmi00@students.oamk.fin3stmi00@students.oamk.fi">n3stmi00@students.oamk.fi</a>
+              </li>
+              <li className="contact-other p-note">On the contact page</li>
+            </ul>
+            <h2>Date</h2>
+            <p>
+              This statement was created on 26 June 2025 using the <a href="https://www.w3.org/WAI/planning/statements/">W3C Accessibility Statement Generator Tool</a>.
+            </p>
+          </div>
+        </div>
+    </>
   )
 }
 
